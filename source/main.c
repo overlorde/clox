d99802a99a6d78027a716f2ccc9500997bf52a8e
Farhan Saif <farhansaif488@gmail.com>
2022-05-06 23:24:14 +0000

    14.4 done

---
+++ source/main.c
@@ -0,0 +1,14 @@
+#include "../include/common.h"
+#include "../include/chunk.h"
+#include "../include/debug.h"
+
+int main(int argc, const char* argv[]){
+	Chunk chunk;
+	initChunk(&chunk);
+	writeChunk(&chunk, OP_RETURN);
+
+	disassembleChunk(&chunk, "test chunk");
+	freeChunk(&chunk);
+	return 0;
+}
+

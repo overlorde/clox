eda9c260312c740ad1338c54d30cecbe87a49dbb
Farhan Saif <farhansaif488@gmail.com>
2022-05-07 12:18:29 +0000

    14 done

--- source/main.c
+++ source/main.c
@@ -5,10 +5,21 @@
 int main(int argc, const char* argv[]){
 	Chunk chunk;
 	initChunk(&chunk);
-	writeChunk(&chunk, OP_RETURN);
 
+	int constant1 = addConstant(&chunk, 1.2);
+	int constant2 = addConstant(&chunk, 2.4);
+
+	writeChunk(&chunk, OP_CONSTANT, 123);
+	writeChunk(&chunk, constant1, 123);
+
+
+	writeChunk(&chunk, OP_CONSTANT, 124);
+	writeChunk(&chunk, constant2, 124);
+
+	writeChunk(&chunk, OP_RETURN, 125);
 	disassembleChunk(&chunk, "test chunk");
 	freeChunk(&chunk);
+
 	return 0;
 }
 

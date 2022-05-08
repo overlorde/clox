7addd932814132460861d6339749871a76e08c3c
Farhan Saif <farhansaif488@gmail.com>
2022-05-08 15:37:07 +0000

    rebase

--- source/vm.c
+++ source/vm.c
@@ -2,6 +2,8 @@
 
 #include "../include/common.h"
 #include "../include/vm.h"
+#include "../include/debug.h"
+
 #include <stdint.h>
 
 VM vm;
@@ -20,6 +22,11 @@ static InterpretResult run(){
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
 
 	for(;;){
+	#ifdef DEBUG_TRACE_EXECUTION
+		disassembleInstruction(vm.chunk, (int)(vm.ip - vm.chunk->code));
+
+	#endif
+
 		uint8_t instruction;
 
 		switch(instruction = READ_BYTE()){

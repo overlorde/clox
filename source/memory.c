e28ea150ee0298c8ebf6e34a8029f32cf53c545f
Farhan Saif <farhansaif488@gmail.com>
2022-05-22 18:21:15 +0000

    18 done with basic expression evaluator

--- source/memory.c
+++ source/memory.c
@@ -5,12 +5,12 @@
 
 
 void* reallocate(void* pointer, size_t oldSize, size_t newSize){
-	if(newSize == 0){
-		free(pointer);
-		return NULL;
-	}
-	void* result = realloc(pointer, newSize);
-	if(result == NULL) exit(1);
-	return result;
+    if(newSize == 0){
+	free(pointer);
+	return NULL;
+    }
+    void* result = realloc(pointer, newSize);
+    if(result == NULL) exit(1);
+    return result;
 }
 
